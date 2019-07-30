6910abf21a77ca81bcc76ee46fc0ea78a6d9ab7b
aisen <ck253@mail.missouri.edu>
2019-07-30 12:18:45 +0900

    from mac

---
+++ src/java2_Generics/BoxDemo.java
@@ -0,0 +1,19 @@
+//package java2_Generics;
+//
+//public class BoxDemo {
+//
+//    public static <U> void addBox(U u. List<Box<U>> boxes){
+//        Box<U> vox = new Box<>();
+//        box.set(u);
+//        boxes.add(box);
+//    }
+//
+//    public static <U> voud outputBoxes(List<Box<U>> boxes) {
+//        int counter = 0;
+//        for (Box<U> box : boxes) {
+//            U boxContents = box.get();
+//            System.out.println("Box #" + counter  "contains [" + boxContents.toString());
+//        }
+//    }
+//
+//}
